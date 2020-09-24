fb8adb356c90932654d140862885ede2c593de73
Emmanuel Sandoval <macchie-23@hotmail.com>
2020-09-24 17:51:04 -0600

    Commit Inicial

--- js/custom.js
+++ js/custom.js
@@ -66,3 +66,60 @@ $('#return-to-top').click(function () {
         scrollTop: 0 // Scroll to top of body
     }, 500);
 });
+
+/* ============================================================
+   Efecto Imagenes
+ * ============================================================*/
+function openModal() {
+    document.getElementById("myModal").style.display = "inline-grid";
+    document.getElementById("return-to-top").style.display = "none"
+    document.getElementById("return-to-top").style.property="value"
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";   
+    document.getElementById("return-to-top").style.display = "block"
+    document.getElementById("return-to-top").style.property="value"   
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }  
+ 
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+    
+  }
+  
+// document.querySelectorAll(".modal-container img").forEach(el=>{el.addEventListener("click",function(ev){
+//         ev.stopPropagation();
+//         this.parentNode.classList.add("active");
+
+//     })
+// });
+
+// document.querySelectorAll(".modal-container img").forEach(el=>{el.addEventListener("click",function(ev){
+//     this.parentNode.classList.remove("active");
+// })
+// });
